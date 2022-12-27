125ec6dd6d347ef4afcc56daf5dccde39179e9c2
ekjotsinghmakol <109962748+ekjotsinghmakol@users.noreply.github.com>
2022-12-27 14:29:26 +0530

    Add files via upload

---
+++ Newproject/src/com/bhavna/main/Main.java
@@ -0,0 +1,13 @@
+package com.bhavna.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Prefix solution = new Prefix();
+		String[] b = { "geeksforgeeks", "geeks", "geek", "geezer" };
+		System.out.println("Longest prefix:" + solution.common(b));
+
+	}
+
+}
---
+++ Newproject/src/com/bhavna/main/Prefix.java
@@ -0,0 +1,17 @@
+package com.bhavna.main;
+
+public class Prefix {
+	public String common(String[] strs) {
+		if (strs.length == 0)
+			return "";
+		
+		String a = strs[0];
+		
+		for(int i=1;i<strs.length;i++) {
+			while(strs[i].indexOf(a) !=0) {
+				a = a.substring(0, a.length()-1);		
+			}
+		}
+		return a;
+	}
+}
